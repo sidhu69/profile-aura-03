2574943403862d269de45effed8d710d6c90bf89
Noob Ansh <71966507+sidhu69@users.noreply.github.com>
2025-10-03 20:48:48 +0530

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/core';
+
+const config: CapacitorConfig = {
+  appId: 'com.sidhu69.profileaura',
+  appName: 'Profile Aura',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
